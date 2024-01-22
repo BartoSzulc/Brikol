cf512249e21807336a809f67b5a6cedfe6038645
BartoszSzulcTamago <bartosz.szulc@tamago.software>
2024-01-22 14:39:50 +0100

    test

--- resources/views/sections/home/hero.blade.php
+++ resources/views/sections/home/hero.blade.php
@@ -3,7 +3,7 @@ $data = get_field('hero');
 @endphp
 
 @if (!empty($data))
-<section class="home__hero overflow-hidden">
+<section class="home__hero overflow-hidden bg-purple-500">
     <div class="container relative">
         <div class="swiper heroSwiper">
             <div class="swiper-wrapper">
